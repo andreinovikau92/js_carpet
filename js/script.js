ded10a0231937eb80ab7f9f25d11e06827348349
Andrei <andrei.novikau92@gmail.com>
2021-09-28 15:59:40 +0300

    My first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict"; // Современный режим
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}`); // Приём интерполяции ``- бэктики (обратные ковычки)
+
+const user = 'Andrei';
+
+alert(`Hello ${user}` );
